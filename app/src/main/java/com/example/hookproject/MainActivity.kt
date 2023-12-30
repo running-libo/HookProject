8d15023e11be68f34ffdd68939643ae3f9d4fedd
libo <417845639@qq.com>
2023-12-30 12:27:13 +0800

    project create

--- app/src/main/java/com/example/hookproject/MainActivity.kt
+++ app/src/main/java/com/example/hookproject/MainActivity.kt
@@ -1,46 +1,20 @@
 package com.example.hookproject
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.hookproject.ui.theme.HookProjectTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            HookProjectTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        var button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            Toast.makeText(this@MainActivity, "" + (it as Button).text, Toast.LENGTH_SHORT).show()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HookProjectTheme {
-        Greeting("Android")
     }
 }
